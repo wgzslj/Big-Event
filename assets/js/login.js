65ce33bae7f9d64a918cee2dc167db9cfd9f80f5
wgz <a18819545917@163.com>
2022-03-17 15:25:38 +0800

    完成登录注册功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -10,4 +10,30 @@ $(function() {
     $('.login-box').show()
     $('.reg-box').hide()
   })
+
+  // 登录请求
+  $('#form_login').on('submit', function(e) {
+    // 1. 阻止默认的提交行为
+    e.preventDefault()
+    // 2. 发起Ajax的POST请求
+    var data = {
+      username: $('#form_reg [name=username]').val(),
+      password: $('#form_reg [name=password]').val()
+    }
+    console.log('获取数据成功')
+    console.log(data)
+  })
+
+  // 注册请求
+  $('#form_reg').on('submit', function(e) {
+    // 1. 阻止默认的提交行为
+    e.preventDefault()
+    // 2. 发起Ajax的POST请求
+    var data = {
+      username: $('#form_reg [name=username]').val(),
+      password: $('#form_reg [name=password]').val()
+    }
+    console.log('获取数据成功')
+    console.log(data)
+  })
 })
